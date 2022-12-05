03adabcac3f652d7d9dad9da3b778fed6dc7e36a
Vijay Danda <vidanda@med.umich.edu>
2022-12-05 13:21:18 -0500

    test: add test for react select and multi select
    react seelct and multi select components should be validated on submit and onchnage
    
    closes #32

--- test/src/cypress/components/Form.cy.tsx
+++ test/src/cypress/components/Form.cy.tsx
@@ -99,57 +99,6 @@ describe('Feature: Validated input component should do validation even when onch
   });
 });
 
-describe('Feature: Validated input component should do validation even when onchange function is provided', () => {
-  describe('Scenario: Changing a value with an onchange function should trigger validation', () => {
-    const App = () => {
-      const inputRef = useRef() as LegacyRef<HTMLInputElement>;
-
-      const [valid, setValid] = useState(false);
-      const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-        setValid(true);
-        e.preventDefault();
-        e.stopPropagation();
-      };
-      const ValidatedInput = withValidation('input');
-      return (
-        <section className='add-service'>
-          <Form onSubmit={handleSubmit}>
-            <ValidatedInput
-              name='input'
-              type='text'
-              placeholder='Enter an email'
-              // @ts-ignore
-              ref={inputRef}
-              onChange={() => console.log('hi')}
-              dataValidationRules={{email: {value: true}}}
-            />
-            <br />
-            <button id='buttonSubmitForm'>Submit form</button>
-          </Form>
-          <div id='formStatus'>{valid ? 'Form is valid' : null}</div>
-        </section>
-      );
-    };
-
-    it(`Given form contains an  <input> element with validation rule and onchange function
-     And invalid value is entered
-     Then validation <Error> message should be displayed for <input>
-     And valid value is entered
-     Then validation <Error> message should be displayed for <input>`, () => {
-      cy.mount(<App />);
-      cy.get('#formStatus').should('be.empty');
-
-      cy.get('[name=input]').clear().type('q');
-
-      cy.get('#error-input').should('have.text', 'This value should be a valid email.');
-
-      cy.get('[name=input]').clear().type('ab@cd.com');
-
-      cy.get('#error-input').should('not.exist');
-    });
-  });
-});
-
 describe('Feature: Validated form component should be able to validate components requiring validations by specifying dataValidationRules attribute. (HOC is used to create validated components', () => {
   const errorClassName = 'field-error-text filled';
   const validEmail = 'johndoe@gmail.com',
@@ -160,12 +109,15 @@ describe('Feature: Validated form component should be able to validate component
     invalidReactSelectIndex = 0,
     invalidEmailError = 'This value should be a valid email.',
     invalidSelectNumberError = 'You should have selected a value',
-    invalidReactSelectNumberError = 'Field is required',
+    invalidSelectColorError = 'You should have selected a color',
+    fieldRequiredError = 'Field is required',
     textareaTooLongError = "You shouldn't have entered more than 2 characters.";
   const App = () => {
     const inputRef = useRef();
-    const selectRef = useRef();
-    const reactSelectRef = useRef();
+    const selectRefNumber = useRef();
+    const selectRefColor = useRef();
+    const reactSelectNumberRef = useRef();
+    const reactSelectCarRef = useRef();
     const textareaRef = useRef();
 
     const [valid, setValid] = useState(false);
@@ -194,7 +146,7 @@ describe('Feature: Validated form component should be able to validate component
           <br />
           <ValidatedSelect
             // @ts-ignore
-            ref={selectRef}
+            ref={selectRefNumber}
             name={'selectNumber'}
             dataValidationRules={{required: {value: true, errorMessage: invalidSelectNumberError}}}>
             <option value=''>Choose a number...</option>
@@ -204,17 +156,46 @@ describe('Feature: Validated form component should be able to validate component
             <option value='Four'>4</option>
           </ValidatedSelect>
           <br />
+          <ValidatedSelect
+            // @ts-ignore
+            ref={selectRefColor}
+            name={'selectColor'}
+            dataValidationRules={{required: {value: true, errorMessage: invalidSelectColorError}}}>
+            <option value=''>Choose a color...</option>
+            <option value=''></option>
+            <option value='Green'>Green</option>
+            <option value='Red'>Red</option>
+            <option value='Blue'>Blue</option>
+          </ValidatedSelect>
+          <br />
           <ValidatedReactSelect
             id={'reactSelectNumber'}
             name={'reactSelectNumber'}
-            isMulti
             valueSelector={(val) => (Array.isArray(val) ? val[0]?.id : val?.id)}
             // @ts-ignore
-            ref={reactSelectRef}
+            ref={reactSelectNumberRef}
             options={[
               {text: 'Select one option...', id: undefined},
               {text: 'One', id: '1'},
               {text: 'Two', id: '2'}
+            ]}
+            // @ts-ignore
+            getOptionLabel={(option) => option.text}
+            // @ts-ignore
+            getOptionValue={(option) => option.id}
+            dataValidationRules={{required: {value: true}}}
+          />
+          <ValidatedReactSelect
+            id={'reactSelectCar'}
+            name={'reactSelectCar'}
+            valueSelector={(val) => (Array.isArray(val) ? val[0]?.id : val?.id)}
+            isMulti
+            // @ts-ignore
+            ref={reactSelectCarRef}
+            options={[
+              {text: 'Select an option...', id: undefined},
+              {text: 'Honda', id: '1'},
+              {text: 'Ford', id: '2'}
             ]}
             // @ts-ignore
             getOptionLabel={(option) => option.text}
@@ -251,18 +232,18 @@ describe('Feature: Validated form component should be able to validate component
       And <textarea/> element with a maxLength validation rule
       And error message class is passed as ${errorClassName}
       When <Form> is submitted
-      Then <Error> messages should be displayed for <input/>: ${invalidEmailError}, <select/>: ${invalidSelectNumberError}, <textarea/>: ${invalidReactSelectNumberError} and <react-select/>: ${textareaTooLongError}
+      Then <Error> messages should be displayed for <input/>: ${invalidEmailError}, <select/>: ${invalidSelectNumberError}, <textarea/>: ${fieldRequiredError} and <react-select/>: ${textareaTooLongError}
        And Error messages should have ${errorClassName} as classname `, () => {
       cy.mount(<App />);
 
       cy.get('#reactSelectNumber').reactSelect(invalidReactSelectIndex);
+      cy.get('[name=selectColor]').select(2);
       cy.get('#buttonSubmitForm').click();
 
       cy.get('#error-email').should('not.exist');
-      // cy.get('#error-email').should('have.text', invalidEmailError);
-      // cy.get('#error-email').should('have.class', errorClassName);
       cy.get('#error-selectNumber').should('have.text', invalidSelectNumberError);
-      cy.get('#error-reactSelectNumber').should('have.text', invalidReactSelectNumberError);
+      cy.get('#error-reactSelectNumber').should('have.text', fieldRequiredError);
+      cy.get('#error-reactSelectCar').should('have.text', fieldRequiredError);
       cy.get('#error-textarea').should('have.text', textareaTooLongError);
     });
   });
@@ -273,7 +254,7 @@ describe('Feature: Validated form component should be able to validate component
                And required <select/> element
                And error message ${invalidSelectNumberError} is displayed
                And required <reactSelect/> element
-               And error message ${invalidReactSelectNumberError} is displayed
+               And error message ${fieldRequiredError} is displayed
               When ${validEmail} is typed in <input/>
                And <select/> selected the option with index ${validSelectIndex}
                And <reactSelect/> selected the option with index ${validReactSelectIndex}
@@ -288,11 +269,15 @@ describe('Feature: Validated form component should be able to validate component
       cy.get('[name=email]').clear().type(validEmail);
       cy.get('#error-email').should('not.exist');
 
+      cy.get('#error-selectColor').should('have.text', invalidSelectColorError);
+      cy.get('[name=selectColor]').select(2);
+      cy.get('#error-selectColor').should('not.exist');
+
       cy.get('#error-selectNumber').should('have.text', invalidSelectNumberError);
       cy.get('[name=selectNumber]').select(validSelectIndex);
       cy.get('#error-selectNumber').should('not.exist');
 
-      cy.get('#error-reactSelectNumber').should('have.text', invalidReactSelectNumberError);
+      cy.get('#error-reactSelectNumber').should('have.text', fieldRequiredError);
       cy.get('#reactSelectNumber').reactSelect(validReactSelectIndex);
       cy.get('#error-reactSelectNumber').should('not.exist');
     });
@@ -307,25 +292,33 @@ describe('Feature: Validated form component should be able to validate component
              And <reactSelect/> selected the option with index ${invalidReactSelectIndex}
             Then Error messages must be displayed for <input/> as ${invalidEmailError}
              And for <select/> as ${invalidSelectNumberError}
-             And for <reactSelect/> as ${invalidReactSelectNumberError}.`, () => {
+             And for <reactSelect/> as ${fieldRequiredError}.`, () => {
       cy.mount(<App />);
 
       cy.get('[name=email]').clear().type(validEmail);
       cy.get('#error-email').should('not.exist');
-
       cy.get('[name=email]').clear().type(invalidEmail);
       cy.get('#error-email').should('have.text', invalidEmailError);
 
       cy.get('[name=selectNumber]').select(validSelectIndex);
       cy.get('#error-selectNumber').should('not.exist');
-
       cy.get('[name=selectNumber]').select(invalidSelectIndex);
       cy.get('#error-selectNumber').should('have.text', invalidSelectNumberError);
+
+      cy.get('[name=selectColor]').select(2);
+      cy.get('#error-selectColor').should('not.exist');
+      cy.get('[name=selectColor]').select(1);
+      cy.get('#error-selectColor').should('have.text', invalidSelectColorError);
 
       cy.get('#reactSelectNumber').reactSelect(validReactSelectIndex);
       cy.get('#error-reactSelectNumber').should('not.exist');
       cy.get('#reactSelectNumber').reactSelect(invalidReactSelectIndex);
-      cy.get('#error-reactSelectNumber').should('have.text', invalidReactSelectNumberError);
+      cy.get('#error-reactSelectNumber').should('have.text', fieldRequiredError);
+
+      cy.get('#reactSelectCar').reactSelect(validReactSelectIndex);
+      cy.get('#error-reactSelectCar').should('not.exist');
+      cy.get('*[class^="css-xb97g8"]').click({multiple: true});
+      cy.get('#error-reactSelectCar').should('have.text', fieldRequiredError);
     });
   });
 });
